1e5c742d63f59b8833eb85ffcee94d6112843e4a
xbowery <69230356+xbowery@users.noreply.github.com>
2021-09-22 14:02:14 +0800

    Initialise Project

---
+++ APICode/src/main/java/com/app/APICode/G2T4Application.java
@@ -0,0 +1,12 @@
+package com.app.APICode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2T4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2T4Application.class, args);
+	}
+}
---
+++ APICode/src/test/java/com/app/APICode/G2T4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.APICode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2T4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
