50b1c88faf7e54bf526e12ce54bc3197c8d20db8
Joseph Teo <joseph1998@hotmail.sg>
2021-10-04 00:28:58 +0800

    Created a on click for home
    - Able to click on gatherings to open a modal (pop up window)
    - click anywhere outside of the window to close it

--- FrontEnd/src/views/Home.vue
+++ FrontEnd/src/views/Home.vue
@@ -1,12 +1,58 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+      <v-card
+        flat
+        class="py-12"
+      >
+        <v-card-text>
+          <v-row
+            align="center"
+            justify="center"
+          >
+            <v-btn-toggle>
+              <v-btn @click="toggleModal">
+                Gatherings
+              </v-btn>
+              <v-btn>
+                Gym
+              </v-btn>
+              <v-btn>
+                Events
+              </v-btn>
+              <v-btn>
+                Dining
+              </v-btn>
+            </v-btn-toggle>
+          </v-row>
+        </v-card-text>
+      </v-card>
+
+    <div v-if="showModal">
+      <Modal :heading="heading" :text="text" theme="sale" @close="toggleModal" />
+    </div>
   </div>
 </template>
 
 <script>
+import Modal from '../components/Modal'
+
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    Modal
+  },
+  data(){
+      return {
+      heading: 'Gatherings',
+      text: 'Max of 2 pax',
+      showModal: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  },
 }
 </script>
