0e28fee7350fffa95b791b5b6880c38f1eac339e
xbowery <69230356+xbowery@users.noreply.github.com>
2021-10-04 15:21:05 +0800

    Update Employee.java
    Added ctests attribute

--- APICode/src/main/java/com/app/APICode/employee/Employee.java
+++ APICode/src/main/java/com/app/APICode/employee/Employee.java
@@ -2,10 +2,12 @@ package com.app.APICode.employee;
 
 import com.app.APICode.user.*;
 import com.app.APICode.restaurant.*;
+import com.app.APICode.ctest.*;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -29,8 +32,6 @@ public class Employee extends User {
     @Column(name = "employee_id")
     private Long id;
     
-    
-
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
@@ -39,5 +40,6 @@ public class Employee extends User {
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
 
-    
+    @OneToMany(mappedBy = "employee", orphanRemoval = true, cascade=CascadeType.ALL)
+    private List<Ctest> ctests;
 }
