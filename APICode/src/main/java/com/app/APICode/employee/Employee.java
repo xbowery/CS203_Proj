e078af8ae665082c1b4e1f9c3325afc2346e88b4
xbowery <69230356+xbowery@users.noreply.github.com>
2021-10-04 15:30:04 +0800

    Update Employee.java
    - Removed unnecessary imports

--- APICode/src/main/java/com/app/APICode/employee/Employee.java
+++ APICode/src/main/java/com/app/APICode/employee/Employee.java
@@ -5,11 +5,9 @@ import com.app.APICode.restaurant.*;
 import com.app.APICode.ctest.*;
 
 import java.util.List;
-import java.util.Collection;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,12 +15,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public class Employee extends User {
     private static final long serialVersionUID = 1L;
