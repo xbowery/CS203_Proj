d65001317e3240cfa2aaa105b29cbbaa7e148af5
xbowery <69230356+xbowery@users.noreply.github.com>
2021-10-07 22:24:14 +0800

    Update conflict files

--- APICode/src/main/java/com/app/APICode/ctest/CtestController.java
+++ APICode/src/main/java/com/app/APICode/ctest/CtestController.java
@@ -1,8 +1,11 @@
 package com.app.APICode.ctest;
 
-import java.util.*;
-import org.springframework.web.bind.annotation.*;
-import com.app.APICode.user.*;
+import java.util.List;
+
+import com.app.APICode.user.UserRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CtestController {
@@ -18,25 +21,11 @@ public class CtestController {
     public List<Ctest> getCtest(){
         return ctests.findAll();
     }
-    @GetMapping("/user/{userid}/ctests")
-    public List<Ctest> getAllReviewsByUserName(@PathVariable (value = "userId") String username) {
-        if(!users.findByUsername(username).isPresent()) {
-            throw new UserNotFoundException(username);
-        }
-        return ctests.findByEmployee(users.findByUsername(username).get().getEmployee());
-    }
-
-    /*
-     * add a ctest for a given userId
-     * If there's no such user, throw a UserNotFoundException
-     * 
-     * Return the newly added ctest
-     */
-    @PostMapping("/user/{email}/ctests")
-    public Ctest addCtest(@PathVariable (value = "email") String username, @RequestBody Ctest ctest){
-        return users.findByUsername(username).map(user ->{
-            ctest.setEmployee(user.getEmployee());
-            return ctests.save(ctest);
-        }).orElseThrow(() -> new UserNotFoundException(username));
-    }
+    // @GetMapping("/user/{userid}/ctests")
+    // public List<Ctest> getAllReviewsByUserName(@PathVariable (value = "userId") String username) {
+    //     if(!users.findByUsername(username).isPresent()) {
+    //         throw new UserNotFoundException(username);
+    //     }
+    //     return ctests.findByEmployee(users.findByUsername(username).get().getEmployee());
+    // }
 }
