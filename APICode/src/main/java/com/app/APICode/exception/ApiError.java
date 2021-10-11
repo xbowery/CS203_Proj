548a5baca49e621bfb35ae4cc7fb9897ee868be7
Won Ying Keat <ykwon.2020@scis.smu.edu.sg>
2021-10-11 18:03:32 +0800

    Edited ExceptionHandler to suit spring boot

--- APICode/src/main/java/com/app/APICode/exception/ApiError.java
+++ APICode/src/main/java/com/app/APICode/exception/ApiError.java
@@ -1,15 +1,24 @@
 package com.app.APICode.exception;
 
+import java.util.Date;
+
 public class ApiError {
     final private int status;
     final private String message;
     final private String error;
+    final private Date timestamp;
+    final private String path;
 
-    public ApiError(final int status, final String error, final String message) {
-        super();
+    public ApiError(final int status, final String error, final String message, final String path) {
         this.status = status;
         this.error = error;
         this.message = message;
+        this.path = path;
+        timestamp = new Date();
+    }
+
+    public Date getTimeStamp() {
+        return timestamp;
     }
 
     public int getStatus() {
@@ -22,6 +31,10 @@ public class ApiError {
 
     public String getMessage() {
         return message;
+    }
+
+    public String getPath() {
+        return path;
     }
 
 }
--- APICode/src/main/java/com/app/APICode/exception/RestExceptionHandler.java
+++ APICode/src/main/java/com/app/APICode/exception/RestExceptionHandler.java
@@ -1,14 +1,5 @@
 package com.app.APICode.exception;
 
-import com.app.APICode.ctest.CtestNotFoundException;
-import com.app.APICode.employee.EmployeeNotFoundException;
-import com.app.APICode.measure.MeasureNotFoundException;
-import com.app.APICode.restaurant.RestaurantDuplicateException;
-import com.app.APICode.restaurant.RestaurantNotFoundException;
-import com.app.APICode.user.EmailNotFoundException;
-import com.app.APICode.user.UserNotFoundException;
-import com.app.APICode.user.UserOrEmailExistsException;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +7,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -33,11 +25,12 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
         String message = "";
         for (ObjectError objectError : ex.getBindingResult().getAllErrors()) {
-            message = message + objectError.getDefaultMessage();
+            message = message + objectError.getDefaultMessage() + ". ";
         }
 
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
         ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                message);
+                message, path);
         return new ResponseEntity<Object>(apiError, headers, status);
     }
 
@@ -52,48 +45,10 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
             WebRequest request) {
         String message = ex.getName() + " should be of type " + ex.getRequiredType().getName();
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
 
         ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                message);
-        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
-    }
-
-    /**
-     * Handle our own exceptions
-     * 
-     * @param ex
-     * @param request
-     * @return
-     */
-    @ExceptionHandler({ EmployeeNotFoundException.class, CtestNotFoundException.class, MeasureNotFoundException.class,
-            RestaurantNotFoundException.class, EmailNotFoundException.class, UserNotFoundException.class })
-    public ResponseEntity<Object> handleNotFound(Exception ex, WebRequest request) {
-        String message = ex.getMessage();
-
-        ApiError apiError = new ApiError(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), message);
-        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
-    }
-
-    @ExceptionHandler({ UserOrEmailExistsException.class, RestaurantDuplicateException.class })
-    public ResponseEntity<Object> handleConflict(Exception ex, WebRequest request) {
-        String message = ex.getMessage();
-
-        ApiError apiError = new ApiError(HttpStatus.CONFLICT.value(), HttpStatus.CONFLICT.getReasonPhrase(), message);
-        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
-    }
-
-    /**
-     * Default handler for any other exception not in the list. Can specify more for
-     * more fine grained control.
-     * 
-     * @param ex
-     * @param request
-     * @return
-     */
-    @ExceptionHandler({ Exception.class })
-    public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
-        ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), "An error occurred. Please try again later. ");
+                message, path);
         return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
     }
 
