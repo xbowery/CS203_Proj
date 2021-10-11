5ea21acd6312e654c107354b4453f03bcae74990
Audrey Ang <audrey@aud-2.local>
2021-10-11 15:26:22 +0800

    Update News Entity

--- APICode/src/main/java/com/app/APICode/news/News.java
+++ APICode/src/main/java/com/app/APICode/news/News.java
@@ -1,8 +1,57 @@
 package com.app.APICode.news;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class News {
-    
+    public News() {}
+
+    private @Id @GeneratedValue Long id;
+
+    @NotNull(message = "Source cannot be null")
+    private String source;
+
+    @NotNull(message = "Title cannot be null")
+    private String title;
+
+    @NotNull(message = "Article Date cannot be null")
+    private Date articleDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getArticleDate() {
+        return articleDate;
+    }
+
+    public void setArticleDate(Date articleDate) {
+        this.articleDate = articleDate;
+    }
+
 }
