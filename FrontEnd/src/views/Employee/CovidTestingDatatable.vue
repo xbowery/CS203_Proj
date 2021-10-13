2312e779733f6597b103bb6724484a30f4611ec4
Joseph Teo <joseph1998@hotmail.sg>
2021-10-13 17:36:59 +0800

    Updated the covid testing page, fixed the cancle and save buttons
    - Fixed cancle button
    - Shifted AddTest into the CovidTestingDatatble.vue

--- FrontEnd/src/views/Employee/CovidTestingDatatable.vue
+++ FrontEnd/src/views/Employee/CovidTestingDatatable.vue
@@ -33,7 +33,58 @@
               <span>Add New Test</span>
             </v-btn>
           </template>
-          <add-test></add-test>
+          <v-card>
+            <v-card-title class="text-h5 grey lighten-2"> New Test </v-card-title>
+            <v-form v-model="valid">
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="6">
+                    <v-overflow-btn label="Select Type of Test" :items="dropdown_type"></v-overflow-btn>
+                  </v-col>
+                  <v-col cols="12" md="6">
+                    <v-overflow-btn label="Select Result of Test" :items="dropdown_result"></v-overflow-btn>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12" sm="6">
+                    <v-menu
+                      ref="menu"
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      :return-value.sync="date"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="auto"
+                    >
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                          v-model="date"
+                          label="Day of Test"
+                          prepend-icon="mdi-calendar"
+                          readonly
+                          v-bind="attrs"
+                          v-on="on"
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker v-model="date" no-title scrollable :allowed-dates="disableFutureDates">
+                        <v-spacer></v-spacer>
+                        <v-btn text color="primary" @click="menu = false" v-bind="attrs" v-on="on"> Cancel </v-btn>
+                        <v-btn text color="primary" @click="$refs.menu.save(date)"> OK </v-btn>
+                      </v-date-picker>
+                    </v-menu>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text @click="dialog = false"> Cancel </v-btn>
+              <v-btn color="primary" text @click="dialog = false"> Save </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-dialog>
       </div>
     </v-card-text>
@@ -53,12 +104,21 @@
 <script>
 import { mdiSquareEditOutline, mdiDotsVertical } from '@mdi/js'
 import data from './covidtestingdatatable-data.js'
-import AddTest from './AddTest'
 
 export default {
-  components: {
-    AddTest,
+  data: () => ({
+    dropdown_type: [{ text: 'ART' }, { text: 'PCR' }],
+    dropdown_result: [{ text: 'Pending' }, { text: 'Negative' }, { text: 'Positive' }],
+
+    date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000).toISOString().substr(0, 10),
+    menu: false,
+  }),
+  methods: {
+    disableFutureDates(val) {
+      return val <= new Date().toISOString().substr(0, 10)
+    },
   },
+
   setup() {
     return {
       dialog: false,
