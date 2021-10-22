2d942d024127c187420e875a20814b7a27e9ddca
yuenkm40 <yuenkm40@gmail.com>
2021-10-22 14:04:26 +0800

    Update RestaurantList.vue

---
+++ FrontEnd/src/views/user-management/admin/RestaurantList.vue
@@ -0,0 +1,417 @@
+<template>
+<v-card>
+    <v-card-text class="d-flex align-center flex-wrap pb-0">
+    <!-- search -->
+      <v-text-field
+        v-model="search"
+        placeholder="Search"
+        outlined
+        hide-details
+        dense
+        class="user-search me-3 mb-4"
+        label="Search"
+      >
+      </v-text-field>
+
+      <v-spacer></v-spacer>
+    </v-card-text>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :search = "search"
+    sort-by="location"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Restaurants List</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              Add New Restaurant
+            </v-btn>
+          </template>
+          <validation-observer ref="obs">
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                  <validation-provider name="type" rules="required" v-slot="{ errors }">
+                    <v-text-field
+                      v-model="editedItem.name"
+                      :error-messages="errors[0]"
+                      label="name"
+                    ></v-text-field>
+                  </validation-provider>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                  <validation-provider name="type" rules="required" v-slot="{ errors }">
+                    <v-text-field
+                      v-model="editedItem.location"
+                      :error-messages="errors[0]"
+                      label="location"
+                    ></v-text-field>
+                  </validation-provider>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                  <validation-provider name="type" rules="required" v-slot="{ errors }">
+                    <v-text-field
+                      v-model="editedItem.cuisine"
+                      :error-messages="errors[0]"
+                      label="cuisine"
+                    ></v-text-field>
+                  </validation-provider>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                  <validation-provider name="type" rules="required" v-slot="{ errors }">
+                    <v-text-field
+                      v-model="editedItem.description"
+                      :error-messages="errors[0]"
+                      label="description"
+                    ></v-text-field>
+                  </validation-provider>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                  <validation-provider name="type" rules="required" v-slot="{ errors }">
+                    <v-text-field
+                      v-model="editedItem.maxCapacity"
+                      :error-messages="errors[0]"
+                      label="max capacity"
+                    ></v-text-field>
+                  </validation-provider>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          </validation-observer>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">  
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this user?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template  #[`item.actions`]="{ item }">
+      <!-- <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon> -->
+        <v-icon
+              small
+              color="secondary"
+              outlined
+              class="mr-2"
+
+              @click="editItem(item)"
+            >
+              Edit
+        </v-icon>
+
+        <v-icon
+              small
+              color="secondary"
+              outlined
+              class="mr-2"
+
+              @click="deleteItem(item)"
+            >
+              Delete
+        </v-icon>
+      <!-- <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon> -->
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
+    </v-card>
+</template>
+
+<script>
+import UserService from '@/services/user.service'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
+
+  export default {
+    components: { ValidationProvider, ValidationObserver },
+    data: () => ({
+      dialog: false,
+      dialogDelete: false,
+      search:'',
+      errors:'',
+      roleOptions: [
+      { title: 'Admin', value: 'admin' },
+      { title: 'User', value: 'user' },
+      { title: 'Employee', value: 'employee' },
+      { title: 'Business Owner', value: 'business owner' },
+    ],
+      headers: [
+        {
+          text: 'NAME',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'LOCATION', value: 'location' },
+        { text: 'CUISINE', value: 'cuisine' },
+        { text: 'DESCRIPTION', value: 'description' },
+        { text: 'MAXCAPACITY', value: 'maxCapacity' },
+        { text: 'ACTIONS', value: 'actions', sortable: true },
+      ],
+      items: [],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        location: '',
+        cuisine: '',
+        description: '',
+        maxCapacity: '',
+      },
+      defaultItem: {
+        name: '',
+        lastName: '',
+        location: '',
+        cuisine: '',
+        description: '',
+        maxCapacity: '',
+      },
+    }),
+
+    async mounted() {
+      try {
+        const res = await UserService.getRestaurants()
+        this.items = res.data
+      } catch (error) {
+        console.error(error)
+      }
+    },
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Restaurant' : 'Edit Restaurant'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.desserts = [
+          {
+            Name: 'Frozen',
+            lastName: 'Yogurt',
+            location: 159,
+            cuisine: 6.0,
+            description: 24,
+            maxCapacity: 4.0,
+          },
+          {
+            Name: 'Ice cream',
+            lastName: 'sandwich',
+            location: 237,
+            cuisine: 9.0,
+            description: 37,
+            maxCapacity: 4.3,
+          },
+          {
+            Name: 'Eclair',
+            lastName: null,
+            location: 262,
+            cuisine: 16.0,
+            description: 23,
+            maxCapacity: 6.0,
+          },
+          {
+            Name: 'Cupcake',
+            lastName: null,
+            location: 305,
+            cuisine: 3.7,
+            description: 67,
+            maxCapacity: 4.3,
+          },
+          {
+            Name: 'Gingerbread',
+            lastName: '',
+            location: 356,
+            cuisine: 16.0,
+            description: 49,
+            maxCapacity: 3.9,
+          },
+          {
+            Name: 'Jelly',
+            lastName: 'Bean',
+            location: 375,
+            cuisine: 0.0,
+            description: 94,
+            maxCapacity: 0.0,
+          },
+          {
+            Name: 'Lollipop',
+            lastName: null,
+            location: 392,
+            cuisine: 0.2,
+            description: 98,
+            maxCapacity: 0,
+          },
+          {
+            Name: 'Honeycomb',
+            lastName: '',
+            location: 408,
+            cuisine: 3.2,
+            description: 87,
+            maxCapacity: 6.5,
+          },
+          {
+            Name: 'Donut',
+            lastName: null,
+            location: 452,
+            cuisine: 25.0,
+            description: 51,
+            maxCapacity: 4.9,
+          },
+          {
+            Name: 'Kit',
+            lastName: 'Kat',
+            location: 518,
+            cuisine: 26.0,
+            description: 65,
+            maxCapacity: 7,
+          },
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.items.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.items.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.items.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        } else {
+          this.items.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
+  }
+
+</script>
