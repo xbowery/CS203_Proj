5c10559baca008c4100d434a4db738a570d7a544
audreyangg <audrey.ang.2020@sis.smu.edu.sg>
2021-10-25 12:47:41 +0800

    Creation of notification folder

---
+++ APICode/src/main/java/com/app/APICode/notification/Notification.java
@@ -0,0 +1,84 @@
+package com.app.APICode.notification;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import com.app.APICode.user.User;
+
+@Entity
+public class Notification {
+    private @Id @GeneratedValue Long id;
+
+    @NotNull(message = "date cannot be null")
+    private LocalDateTime date;
+
+    @NotNull(message = "text cannot be null")
+    private String text;
+
+    @NotNull(message = "link cannot be null")
+    private URL link;
+
+    @NotNull(message = "seen cannot be null")
+    private boolean isSeen;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    public Notification() {}
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public URL getLink() {
+        return link;
+    }
+
+    public void setLink(URL link) {
+        this.link = link;
+    }
+
+    public boolean isSeen() {
+        return isSeen;
+    }
+
+    public void setSeen(boolean isSeen) {
+        this.isSeen = isSeen;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Notification(LocalDateTime date,String text, URL link, boolean isSeen) {
+        this.date = date;
+        this.text = text;
+        this.link = link;
+        this.isSeen = isSeen;
+    }
+
+}
