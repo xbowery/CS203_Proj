308797efd61a296109b1f51fb5d82e47a9a3efb5
Jerome Ng <jerome2116@hotmail.com>
2021-10-29 15:55:42 +0800

    Updated imports for controller

--- APICode/src/main/java/com/app/APICode/measure/MeasureController.java
+++ APICode/src/main/java/com/app/APICode/measure/MeasureController.java
@@ -7,7 +7,14 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MeasureController {
