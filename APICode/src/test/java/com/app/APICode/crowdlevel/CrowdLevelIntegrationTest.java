a262d072122c3558c9cedef68f28ad81502d07d7
xbowery <69230356+xbowery@users.noreply.github.com>
2021-11-09 22:16:10 +0800

    Update CrowdLevelIntegrationTest.java

--- APICode/src/test/java/com/app/APICode/crowdlevel/CrowdLevelIntegrationTest.java
+++ APICode/src/test/java/com/app/APICode/crowdlevel/CrowdLevelIntegrationTest.java
@@ -1,5 +1,38 @@
 package com.app.APICode.crowdlevel;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.app.APICode.templates.LoginDetails;
+import com.app.APICode.templates.TokenDetails;
+import com.app.APICode.user.User;
+import com.app.APICode.user.UserRepository;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import io.restassured.RestAssured;
+import io.restassured.config.JsonConfig;
+import io.restassured.path.json.JsonPath;
+import io.restassured.path.json.config.JsonPathConfig;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.config.RedirectConfig.redirectConfig;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(Lifecycle.PER_CLASS)
 public class CrowdLevelIntegrationTest {
     
 }
