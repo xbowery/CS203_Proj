213dd352749c521688a669e8214e7ff027bee2f5
xbowery <69230356+xbowery@users.noreply.github.com>
2021-11-09 15:48:09 +0800

    Update RestaurantServiceTest.java
    fixed wrong import

--- APICode/src/test/java/com/app/APICode/restaurant/RestaurantServiceTest.java
+++ APICode/src/test/java/com/app/APICode/restaurant/RestaurantServiceTest.java
@@ -1,9 +1,9 @@
 package com.app.APICode.restaurant;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
