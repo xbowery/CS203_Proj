571824d3eb5aa4026e58ce95a9cbc35192445af6
Joseph Teo <joseph1998@hotmail.sg>
2021-11-09 13:13:48 +0800

    linked reset button to the backend

--- FrontEnd/src/views/dashboard/CrowdLevel.vue
+++ FrontEnd/src/views/dashboard/CrowdLevel.vue
@@ -2,16 +2,16 @@
   <v-card>
     <v-item-group>
       <v-container>
-        <v-row justify="end" align="left">
-          <v-btn depressed color="normal" large @click="reset()">Reset</v-btn>
-        </v-row>
+
 
         <v-row justify="center" align="center">
-          <v-col v-for="n in 1" :key="n" cols="8" md="4">
+          <v-col  cols="6" md="4">
+            <!-- <h2>Capacity: {{ current }}/{{ maxCapacity }}</h2> -->
             <h2>Capacity: {{ updatedCrowd.noOfCustomers }}/{{ restaurant.maxCapacity }}</h2>
           </v-col>
 
-          <v-col v-for="n in 1" :key="n" cols="9" md="5">
+          <v-col cols="6" md="4">
+            <!-- <h2>Crowd level: {{ crowdLvl }}</h2> -->
             <h2>Crowd level: {{ updatedCrowd.latestCrowd }}</h2>
           </v-col>
         </v-row>
@@ -33,6 +33,14 @@
             <v-btn depressed color="error" large @click="increment(-2)">-2</v-btn>
           </v-col>
         </v-row>
+
+        <v-row justify="center" align="center">
+          <v-btn depressed color="success" large @click="reset()">Reset</v-btn>
+        </v-row>
+
+        <v-spacer></v-spacer>
+        
+
       </v-container>
     </v-item-group>
   </v-card>
@@ -73,7 +81,7 @@ export default {
       if (this.restaurant.crowdLevel != null) {
         var crowdLevel = this.restaurant.crowdLevel
         var latestCrowd = crowdLevel[0]
-        crowdLevel.forEach(item => {
+        crowdLevel.forEach((item) => {
           if (item.datetime > latestCrowd.datetime) {
             latestCrowd = item
           }
@@ -89,8 +97,8 @@ export default {
 
   methods: {
     reset(){
-      this.items.length = 0
-      this.updatedCrowd.noOfCustomers = 0
+      // this.updatedCrowd.noOfCustomers = 0
+      this.increment(-1*this.updatedCrowd.noOfCustomers)
     },
 
     increment(count) {
@@ -106,6 +114,7 @@ export default {
         this.handlePostCrowdlevel(newCrowd)
         //then a new get request
         this.handleGetCrowdlevel()
+        console.log(this.updatedCrowd)
       }
     },
     getNewCrowd(count) {
