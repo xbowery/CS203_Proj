550ac08eaeb3c4743c8509a95185eddf9554648e
Jerome Ng <jerome2116@hotmail.com>
2021-11-11 21:23:04 +0800

    NotificationController Swagger Documentation

--- APICode/src/main/java/com/app/APICode/user/UserController.java
+++ APICode/src/main/java/com/app/APICode/user/UserController.java
@@ -9,7 +9,6 @@ import javax.validation.Valid;
 
 import com.app.APICode.user.message.ChangePasswordMessage;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
